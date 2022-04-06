4dac1032da24b3111d1fcbf4e5d033534227142b
Robert Adams <adamsiii.robert@gmail.com>
2022-04-06 15:47:10 -0700

    added starter file

---
+++ TestExpression.java
@@ -0,0 +1,11 @@
+import static java.lang.Math.*;
+
+@SuppressWarnings("unused")
+public class TestExpression {
+    public static void main(String[] args) {
+        double x = 3;
+        double y = 4;
+        double z = ( 0 ) - 1 % sin ( 42 + x );
+        System.out.println(z);
+    }
+}
